851cd4fc4a2eaf0ff2ae842b7b103ca26cf5c3d2
lubiao <1368321570@qq.com>
2018-08-03 10:08:23 +0800

    Initial commit

---
+++ src/main/java/com/mmall/concurrrency/ConcurrrencyApplication.java
@@ -0,0 +1,12 @@
+package com.mmall.concurrrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrrencyApplication.class, args);
+	}
+}
