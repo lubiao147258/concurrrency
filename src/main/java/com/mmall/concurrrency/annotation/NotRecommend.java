177b0d6e4ea12cfb89e0f44e60ac8cc0bd0660e6
lubiao <1368321570@qq.com>
2018-08-03 10:21:01 +0800

    添加四个注解

---
+++ src/main/java/com/mmall/concurrrency/annotation/NotRecommend.java
@@ -0,0 +1,19 @@
+package com.mmall.concurrrency.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记【不推荐】的类或者写法
+ * @author lubiao
+ * @createDate 2018年08月03日 上午 10:15:00
+ */
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+
+    String value() default "";
+}
